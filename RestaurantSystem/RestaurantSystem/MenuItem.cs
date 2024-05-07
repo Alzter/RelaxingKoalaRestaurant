0946352f0544e006ce8651df2c92306276c54ad4
Alzter <alexsmudgy20@gmail.com>
2024-05-07 11:29:10 +1000

    Add ingredients to menu items

--- RestaurantSystem/RestaurantSystem/MenuItem.cs
+++ RestaurantSystem/RestaurantSystem/MenuItem.cs
@@ -9,10 +9,12 @@ namespace RestaurantSystem
     public class MenuItem : MenuElement
     {
         private float basePrice;
+        private List<Ingredient> ingredients;
 
-        public MenuItem(float basePrice)
+        public MenuItem(float basePrice, List<Ingredient> ingredients)
         {
             this.basePrice = basePrice;
+            this.ingredients = ingredients;
         }
 
         private List<MenuElement> _items;
@@ -21,5 +23,12 @@ namespace RestaurantSystem
         {
             get { return basePrice; }
         }
+
+        public List<Ingredient> Ingredients { get { return ingredients; } }
+
+        public void AddIngredient(Ingredient item) { ingredients.Add(item); }
+
+        public void RemoveIngredient(Ingredient item) { ingredients.Remove(item); }
+        public void RemoveIngredient(int index) { ingredients.RemoveAt(index); }
     }
 }
