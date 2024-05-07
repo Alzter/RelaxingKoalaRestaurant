b1b99508e7f6052db4a7c2dffe40fb2a5d738ba3
Alzter <alexsmudgy20@gmail.com>
2024-05-07 11:10:45 +1000

    Add empty class files and terrible AI images

---
+++ RestaurantSystem/RestaurantSystem/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem
+{
+    public abstract class Menu
+    {
+        private List<MenuItem> items;
+
+        public Menu()
+        {
+
+        }
+    }
+}
