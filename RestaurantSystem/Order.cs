11656ca1c1ccfd089594a24c1d785f7f2fe43e61
Alzter <alexsmudgy20@gmail.com>
2024-05-09 13:29:13 +1000

    Progress on the Order class

--- RestaurantSystem/Order.cs
+++ RestaurantSystem/Order.cs
@@ -8,5 +8,56 @@ namespace RestaurantSystem
 {
     public abstract class Order
     {
+        public enum OrderStatus
+        {
+            Waiting,
+            InProgress,
+            Ready,
+            Served
+        }
+
+        private bool _isPaid;
+        private List<MenuItem> _items;
+        private DateTime _creationTime;
+        private DateTime _completionTime;
+        private DateTime _estimatedCompletionTime;
+        private OrderStatus _status;
+
+        public Order(List<MenuItem> items)
+        {
+            _items = items;
+            _isPaid = false;
+            _status = OrderStatus.Waiting;
+            _creationTime = DateTime.Now;
+        }
+
+        public OrderStatus Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public List<MenuItem> Items
+        {
+            get { return _items; }
+        }
+
+        public float Price
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsPaid
+        {
+            get{ return _isPaid; }
+        }
+
+        public Invoice GetInvoice()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
