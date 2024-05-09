c1096f3736be50f32e6e032df82abd8856437174
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-09 13:16:46 +1000

    Windows Forms Interfaces Set Up
    - Set up the basic interfaces

---
+++ RestaurantSystem/WaitStaffInterface.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace RestaurantSystem
+{
+    public partial class WaitStaffInterface : Form
+    {
+        public WaitStaffInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
