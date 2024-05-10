506fb740b3373158d74e84ca5469f4fdffe6d85f
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-10 17:31:21 +1000

    Table - Basic Functionality
    - Constructor, table number, and status variables done
    - function to update status done

--- RestaurantSystem/Table.cs
+++ RestaurantSystem/Table.cs
@@ -4,10 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public enum TableStatus
+{
+    EMPTY,
+    OCCUPIED,
+    RESERVED,
+    NEEDS_CLEANING
+}
+
 namespace RestaurantSystem
 {
     public class Table
     {
+        private int _tableNumber;
+        private TableStatus _status = TableStatus.EMPTY;
 
+        public Table(int tableNumber)
+        {
+            _tableNumber = tableNumber;
+        }
+
+        public void ChangeStatus(TableStatus status)
+        {
+            _status = status;
+        }
     }
 }
