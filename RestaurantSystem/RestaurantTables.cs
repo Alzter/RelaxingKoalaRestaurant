fcef96388911d18d5f772ba37abe7a84a2114be4
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-10 17:28:19 +1000

    Created Table and RestaurantTables
    - Stubs for now

---
+++ RestaurantSystem/RestaurantTables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem
+{
+    public class RestaurantTables
+    {
+        private List<Table> _tables;
+
+        public RestaurantTables()
+        {
+            _tables = new List<Table>();
+
+        }
+    }
+}
