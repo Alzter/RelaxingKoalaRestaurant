95ecebf3c4e7d45225b3dbdff14a66bc213e02ec
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-10 17:35:01 +1000

    RestaurantTables - Singleton
    - Singleton pattern implemented (not thread safe - idk if we need to worry about that in this project?)

--- RestaurantSystem/RestaurantTables.cs
+++ RestaurantSystem/RestaurantTables.cs
@@ -8,12 +8,24 @@ namespace RestaurantSystem
 {
     public class RestaurantTables
     {
+        private static RestaurantTables _instance;
         private List<Table> _tables;
+
+        public static RestaurantTables Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new RestaurantTables();
+                }
+                return _instance;
+            }
+        }
 
         public RestaurantTables()
         {
             _tables = new List<Table>();
-
         }
     }
 }
