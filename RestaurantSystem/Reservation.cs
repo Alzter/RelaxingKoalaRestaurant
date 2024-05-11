4c1920beca0d5bdc1d2030dc41fedf3ee74c9c89
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-11 12:01:55 +1000

    Reservation Created
    - Created Reservation class
    - Added basic functionality

---
+++ RestaurantSystem/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem
+{
+    public class Reservation
+    {
+        private DateTime _bookingTime;
+        private Table _tableReserved;
+        // Customer?
+        // Number of guests?
+
+        public Reservation(DateTime bookingTime)
+        {
+            _bookingTime = bookingTime;
+            _tableReserved = null; // Will work out how table is assigned later
+        }
+
+        public void ReserveTable(Table table)
+        {
+            _tableReserved = table;
+        }
+    }
+}
