37275075fc323caa45e5327381ec90f7fc26d812
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-11 11:54:55 +1000

    CustomerReservations Created
    - Created class
    - Implemented singleton pattern

---
+++ RestaurantSystem/CustomerReservations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem
+{
+    public class CustomerReservations
+    {
+        private static CustomerReservations _instance;
+
+        private CustomerReservations() { }
+
+        public static CustomerReservations Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CustomerReservations();
+                }
+                return _instance;
+            }
+        }
+    }
+}
