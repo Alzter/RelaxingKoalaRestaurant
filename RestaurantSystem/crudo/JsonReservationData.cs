bad0aa605c0e6b1d0e2c0217e4a89b6382c3d720
erikfanning <132543883+erikfanning@users.noreply.github.com>
2024-05-28 11:56:53 +1000

    Created JsonReservationData.cs for mapping reservation data to json file and vice versa

---
+++ RestaurantSystem/crudo/JsonReservationData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestaurantSystem.crudo
+{
+    public class JsonReservationData
+    {
+        [JsonPropertyName("reservations")]
+        public List<JsonReservation> Reservations { get; set; }
+    }
+
+    public class JsonReservation
+    {
+        [JsonPropertyName("startTime")]
+        public DateTime StartTime { get; set; }
+
+        [JsonPropertyName("durationMinutes")]
+        public int DurationMinutes { get; set; }
+
+        [JsonPropertyName("tableNumber")]
+        public int TableNumber { get; set; }
+
+        [JsonPropertyName("customerName")]
+        public string CustomerName { get; set; }
+
+        [JsonPropertyName("numberOfGuests")]
+        public int NumberOfGuests { get; set; }
+    }
+}
