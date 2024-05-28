564d3b0aa63f3ac1fb336a2f7b2b4df34952916d
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-28 10:43:59 +1000

    ReservationService A
    - Created skeleton of class

--- RestaurantSystem/Services/ReservationService.cs
+++ RestaurantSystem/Services/ReservationService.cs
@@ -8,5 +8,24 @@ namespace RestaurantSystem
 {
     public class ReservationService
     {
+        private List<Reservation> _reservations;
+        private List<Reservation> _pastReservations;
+
+        public ReservationService() { }
+
+        public List<Reservation> Reservations { get { return _reservations; } }
+        public List<Reservation> PastReservations { get { return _pastReservations; } }
+
+        public void AddReservation(DateTime startTime, int durationMinutes, int tableNum, string customerName, int numOfGuests) 
+        {
+            // Create Reservation object using CRUDO interface
+        }
+        
+        // QUESTION: Is this used to cancel a reservation? Or to move a completed reservation to the past reservations list?
+        public void RemoveReservation(Reservation r)
+        {
+            // Find reservation in Reservations list?
+            // Add to past reservations? Or delete it?
+        }
     }
 }
