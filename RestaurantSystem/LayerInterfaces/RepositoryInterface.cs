35c6e765dd70d838fa4a674aa965ebac6429fa57
erikfanning <132543883+erikfanning@users.noreply.github.com>
2024-05-28 11:19:44 +1000

    Added stub methods to RepositoryInterface

--- RestaurantSystem/LayerInterfaces/RepositoryInterface.cs
+++ RestaurantSystem/LayerInterfaces/RepositoryInterface.cs
@@ -8,5 +8,82 @@ namespace RestaurantSystem
 {
     public static class RepositoryInterface
     {
+        // Menus - Save / Load
+        public static Menu GetMenus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveMenus(List<Menu> menus) 
+        {
+            throw new NotImplementedException();
+        }
+
+        // Reservations - Save / Load
+        public static List<Reservation> GetReservations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveReservations(List<Reservation> reservations) 
+        {
+            throw new NotImplementedException();
+        }
+
+        // Orders - Save / Load
+        public static List<Order> GetOrders() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveOrders(List<Order> orders) 
+        {
+            throw new NotImplementedException();
+        }
+
+        // Transactions - Save / Load
+        public static List<TransactionRecord> GetTransactions() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveTransactions(List<TransactionRecord> transactions) 
+        { 
+            throw new NotImplementedException();
+        }
+
+        // Tables - Save / Load
+        public static List<Table> GetTables() 
+        { 
+            throw new NotImplementedException();
+        }
+
+        public static void SaveTables(List<Table> tables) 
+        { 
+            throw new NotImplementedException();
+        }
+
+        // Reservation - Create 
+        public static void CreateReservation(int tableNumber, DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Order - Create
+        // Delivery
+        public static void CreateOrder(List<MenuItem> menuItems, string address, string name) 
+        {
+            throw new NotImplementedException();
+        }
+        // Dine-In
+        public static void CreateOrder(List<MenuItem> menuItems, int tableNumber)
+        {
+            throw new NotImplementedException();
+        }
+        // TakeAway
+        public static void CreateOrder(List<MenuItem> menuItems, string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
