5b56f52ee48222f00644c0bfc477dcef6ab70cd6
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-28 10:36:04 +1000

    MenuService A
    - Skeleton of class created

--- RestaurantSystem/Services/MenuService.cs
+++ RestaurantSystem/Services/MenuService.cs
@@ -8,5 +8,15 @@ namespace RestaurantSystem
 {
     public class MenuService
     {
+        private Menu _takeAwayMenu;
+        private Menu _dineInMenu;
+
+        public MenuService()
+        {
+
+        }
+
+        public Menu TakeAwayMenu { get { return _takeAwayMenu; } set { _takeAwayMenu = value; } }
+        public Menu DineInMenu { get { return _dineInMenu; } set { _dineInMenu = value; } }
     }
 }
