ba546eaab9ff21eed0cb3250b249f14701dcdfc9
erikfanning <132543883+erikfanning@users.noreply.github.com>
2024-05-28 16:28:54 +1000

    Added JsonTableData.cs for mapping table json data

---
+++ RestaurantSystem/crudo/JsonTableData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestaurantSystem.crudo
+{
+    public class JsonTableData
+    {
+        [JsonPropertyName("tables")]
+        public List<JsonTable> Tables { get; set; }
+    }
+
+    public class JsonTable
+    {
+        [JsonPropertyName("tableNumber")]
+        public int TableNumber { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+    }
+}
