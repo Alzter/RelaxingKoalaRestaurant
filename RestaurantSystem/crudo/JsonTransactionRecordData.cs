f41d6eea38cc6d087cb793ec10628672a033c93a
erikfanning <132543883+erikfanning@users.noreply.github.com>
2024-05-28 15:52:32 +1000

    Created JsonTransactionRecordData

---
+++ RestaurantSystem/crudo/JsonTransactionRecordData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestaurantSystem.crudo
+{
+    public class JsonTransactionRecordData
+    {
+        [JsonPropertyName("transactionRecords")]
+        public List<JsonTransactionRecord> TransactionRecords { get; set; }
+    }
+
+    public class JsonTransactionRecord
+    {
+        [JsonPropertyName("orderID")]
+        public int OrderID { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("dateIssued")]
+        public DateTime DateIssued { get; set; }
+    }
+}
