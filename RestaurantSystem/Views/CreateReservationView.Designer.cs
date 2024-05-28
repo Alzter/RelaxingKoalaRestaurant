e0451d4930e67bc8172cb182991279772fa32f8a
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-28 12:38:20 +1000

    CreateReservationView A
    - Updated UI

--- RestaurantSystem/Views/CreateReservationView.Designer.cs
+++ RestaurantSystem/Views/CreateReservationView.Designer.cs
@@ -29,32 +29,73 @@
         private void InitializeComponent()
         {
             BtnBack = new Button();
+            CalendarBooking = new MonthCalendar();
+            DateTimeSelection = new DateTimePicker();
+            BtnCreateReservation = new Button();
             SuspendLayout();
             // 
             // BtnBack
             // 
-            BtnBack.Location = new Point(11, 418);
+            BtnBack.Location = new Point(11, 305);
             BtnBack.Margin = new Padding(2, 1, 2, 1);
             BtnBack.Name = "BtnBack";
-            BtnBack.Size = new Size(42, 22);
+            BtnBack.Size = new Size(70, 22);
             BtnBack.TabIndex = 3;
             BtnBack.Text = "Back";
             BtnBack.UseVisualStyleBackColor = true;
             BtnBack.Click += BtnBack_Click;
             // 
-            // CreateReservationService
+            // CalendarBooking
+            // 
+            CalendarBooking.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            CalendarBooking.Location = new Point(126, 28);
+            CalendarBooking.MaxDate = new DateTime(2050, 12, 31, 0, 0, 0, 0);
+            CalendarBooking.MaxSelectionCount = 1;
+            CalendarBooking.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            CalendarBooking.Name = "CalendarBooking";
+            CalendarBooking.TabIndex = 4;
+            // 
+            // DateTimeSelection
+            // 
+            DateTimeSelection.CalendarFont = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DateTimeSelection.Checked = false;
+            DateTimeSelection.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DateTimeSelection.Format = DateTimePickerFormat.Time;
+            DateTimeSelection.Location = new Point(162, 202);
+            DateTimeSelection.MaxDate = new DateTime(2050, 12, 31, 0, 0, 0, 0);
+            DateTimeSelection.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            DateTimeSelection.Name = "DateTimeSelection";
+            DateTimeSelection.Size = new Size(163, 29);
+            DateTimeSelection.TabIndex = 5;
+            // 
+            // BtnCreateReservation
+            // 
+            BtnCreateReservation.Location = new Point(179, 252);
+            BtnCreateReservation.Name = "BtnCreateReservation";
+            BtnCreateReservation.Size = new Size(120, 50);
+            BtnCreateReservation.TabIndex = 6;
+            BtnCreateReservation.Text = "Create Reservation";
+            BtnCreateReservation.UseVisualStyleBackColor = true;
+            // 
+            // CreateReservationView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(482, 337);
+            Controls.Add(BtnCreateReservation);
+            Controls.Add(DateTimeSelection);
+            Controls.Add(CalendarBooking);
             Controls.Add(BtnBack);
-            Name = "CreateReservationService";
-            Text = "CreateReservationService";
+            Name = "CreateReservationView";
+            Text = "Create Reservation";
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button BtnBack;
+        private MonthCalendar CalendarBooking;
+        private DateTimePicker DateTimeSelection;
+        private Button BtnCreateReservation;
     }
 }
