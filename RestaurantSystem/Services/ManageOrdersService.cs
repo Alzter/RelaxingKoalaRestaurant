ff70acd7d52aecadf90200d91fa86b1e9f2dd5ad
Alzter <alexsmudgy20@gmail.com>
2024-05-29 16:01:58 +1000

    Implement CreateOrderService, ManageOrderService, ManageTablesService

--- RestaurantSystem/Services/ManageOrdersService.cs
+++ RestaurantSystem/Services/ManageOrdersService.cs
@@ -8,6 +8,29 @@ namespace RestaurantSystem
 {
     public static class ManageOrdersService
     {
+        public static List<Order> GetOrdersByStatus(OrderStatus status)
+        {
+            List<Order> orders = new List<Order>();
+            List<Order> orderList = Orders;
 
+            foreach (Order o in orderList)
+            {
+                if (o.Status == status) { orders.Add(o); }
+            }
+            return orders;
+        }
+
+        public static void SetOrderStatus(Order o, OrderStatus status)
+        {
+            o.Status = status;
+        }
+
+        public static List<Order> Orders
+        {
+            get
+            {
+                return RepositoryInterface.GetOrders();
+            }
+        }
     }
 }
