58608566daec15ef694aa31d7ed52108ddc3a226
Alzter <alexsmudgy20@gmail.com>
2024-05-30 19:34:37 +1000

    ManageOrdersView - implemented except for Payment passing

--- RestaurantSystem/Services/ManageOrdersService.cs
+++ RestaurantSystem/Services/ManageOrdersService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RestaurantSystem.Service
 {
@@ -22,12 +23,28 @@ namespace RestaurantSystem.Service
 
         public static void SetOrderStatus(Order o, OrderStatus status)
         {
+            List<Order> orderList = RepositoryInterface.GetOrders();
+
+            int orderIndex = orderList.FindIndex((Order order) => { return order.ID == o.ID; });
+
             o.Status = status;
+
+            orderList[orderIndex] = o;
+
+            RepositoryInterface.SaveOrders(orderList);
         }
 
         public static void SetOrderETA(Order o, int etaMinutes)
         {
+            List<Order> orderList = RepositoryInterface.GetOrders();
+
+            int orderIndex = orderList.FindIndex((Order order) => { return order.ID == o.ID; });
+
             o.SetETA(etaMinutes);
+
+            orderList[orderIndex] = o;
+
+            RepositoryInterface.SaveOrders(orderList);
         }
 
         public static List<Order> Orders
