44ba1a83709940e91bf1fbc4286503cab0bbc04b
Maddi <maddi.fitzmaurice@gmail.com>
2024-05-30 14:34:05 +1000

    State Functionality
    - Added State class

---
+++ RestaurantSystem/StateFunctionality/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem.StateFunctionality
+{
+    public abstract class State
+    {
+        protected Form _form;
+        protected object _data;
+
+        public State(Form form)
+        {
+            _form = form;
+        }
+
+        public virtual void Enter() { _form.Show(); }
+        public virtual object SendData() { return _data; }
+        public virtual void ReceiveData(object data) { _data = data; }
+        public virtual void Exit() { _form.Hide(); }
+    }
+}
